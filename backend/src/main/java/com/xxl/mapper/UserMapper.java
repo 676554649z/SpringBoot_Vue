48094fee0c25513e8e37c2c3e4e65383857c2d29
676554649z <676554649@qq.com>
2021-07-26 13:37:26 +0800

    firstJob

---
+++ backend/src/main/java/com/xxl/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.xxl.mapper;
+
+import com.xxl.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    public List<User> findUserByName(String userName);
+
+    public List<User> ListUser();
+
+    public List<User> queryPage(Integer startRows);
+
+    public int getRowCount();
+
+    public int insertUser(User user);
+
+    public int delete(int userId);
+
+    public int Update(User user);
+}
+
---
+++ backend/src/main/java/com/xxl/service/UserService.java
@@ -0,0 +1,47 @@
+package com.xxl.service;
+
+
+import com.xxl.pojo.User;
+import com.xxl.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<User> findByName(String userName) {
+        return userMapper.findUserByName(userName);
+    }
+
+    public List<User> queryPage(Integer startRows) {
+        return userMapper.queryPage(startRows);
+    }
+
+    public int getRowCount() {
+        return userMapper.getRowCount();
+    }
+
+    public User insertUser(User user) {
+        userMapper.insertUser(user);
+        return user;
+    }
+
+    public List<User> ListUser(){
+        return userMapper.ListUser();
+    }
+
+    public int Update(User user){
+        return userMapper.Update(user);
+    }
+
+    public int delete(int userId){
+        return userMapper.delete(userId);
+    }
+
+}
